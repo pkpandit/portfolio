4e4cf8e9b8c04215890ec47f6bb1fd64fe8736d5
pkpandit <pkpandit@gmail.com>
2025-09-09 12:04:28 +0530

    test commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import "aos/dist/aos.css";
-import "aos/dist/aos.js";
+import "../node_modules/aos/dist/aos.css";
+import "../node_modules/aos/dist/aos.js";
 
-import "glightbox/dist/css/glightbox.min.css";
-import "glightbox/dist/js/glightbox.min.js";
+import "../node_modules/glightbox/dist/css/glightbox.min.css";
+import "../node_modules/glightbox/dist/js/glightbox.min.js";
 
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper-bundle.min.js";
+import "../node_modules/swiper/swiper-bundle.min.css";
+import "../node_modules/swiper/swiper-bundle.min.js";
 
-import "waypoints/lib/noframework.waypoints.min.js";
-import "imagesloaded/imagesloaded.pkgd.min.js";
-import "isotope-layout/dist/isotope.pkgd.min.js";
+import "../node_modules/waypoints/lib/noframework.waypoints.min.js";
+import "../node_modules/imagesloaded/imagesloaded.pkgd.min.js";
+import "../node_modules/isotope-layout/dist/isotope.pkgd.min.js";
 
 import "./index.css";
 import App from "./App.jsx";
-import "aos/dist/aos.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
