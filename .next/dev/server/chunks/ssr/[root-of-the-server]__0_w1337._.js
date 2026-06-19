feae3d39a43a1c890ede7c271067e94b5f26a97a
pkpandit <pkpandit@gmail.com>
2026-06-19 18:15:29 +0530

    folder created for front-end and back-end

---
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__0_w1337._.js
@@ -0,0 +1,56 @@
+module.exports = [
+"[externals]/next/dist/compiled/next-server/app-page-turbo.runtime.dev.js [external] (next/dist/compiled/next-server/app-page-turbo.runtime.dev.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/compiled/next-server/app-page-turbo.runtime.dev.js", () => require("next/dist/compiled/next-server/app-page-turbo.runtime.dev.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/server/app-render/work-async-storage.external.js [external] (next/dist/server/app-render/work-async-storage.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/server/app-render/work-async-storage.external.js", () => require("next/dist/server/app-render/work-async-storage.external.js"));
+
+module.exports = mod;
+}),
+"[project]/node_modules/@swc/helpers/cjs/_interop_require_default.cjs [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/node_modules/@swc/helpers/cjs/_interop_require_default.cjs', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+"[project]/node_modules/next/dist/server/route-modules/app-page/module.compiled.js [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/node_modules/next/dist/server/route-modules/app-page/module.compiled.js', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+"[project]/node_modules/next/dist/server/route-modules/app-page/vendored/ssr/react-jsx-runtime.js [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/node_modules/next/dist/server/route-modules/app-page/vendored/ssr/react-jsx-runtime.js', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+"[project]/node_modules/next/dist/server/route-modules/app-page/vendored/ssr/react.js [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/node_modules/next/dist/server/route-modules/app-page/vendored/ssr/react.js', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+"[project]/node_modules/next/dist/client/components/handle-isr-error.js [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/node_modules/next/dist/client/components/handle-isr-error.js', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+"[project]/node_modules/next/dist/client/components/builtin/error-styles.js [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/node_modules/next/dist/client/components/builtin/error-styles.js', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+"[project]/node_modules/next/dist/client/components/builtin/global-error.js [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/node_modules/next/dist/client/components/builtin/global-error.js', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+];
